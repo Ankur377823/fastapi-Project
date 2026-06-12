43c11eaa7bce68e83cc63ae6faf6f14c4d4d127c
Ankur377823 <ankurrajput7050@gmail.com>
2026-06-12 15:49:53 +0530

    added middleware and main.py

---
+++ app/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import  Instrumentator
+from app.api import routes_auth, routes_predict
+from app.middleware.logging_middleware import LoggingMiddleware
+from app.core.exceptions import register_exception_handlers
+
+
+app= FastAPI(title='Car Price Prediction API')
+
+#link middleware
+app.add_middleware(LoggingMiddleware)
+
+#link endpoints
+app.include_router(routes_auth.route_handler,tags=['Auth'])
+app.include_router(routes_predict.router,tags=['prediction'])
+
+#monotoring using prometheus
+Instrumentator().instrument(app).expose(app)
+
+register_exception_handlers(app)
